9f65587b9fdd2f75e19a567d23fa4a634bb74cb9
daeho kang <eoghdhdls@naver.com>
2024-04-02 07:22:58 +0900

    [feat] : first Commit

---
+++ src/main/java/org/example/MongoEscalate.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoEscalate {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(MongoEscalate.class, args);
+    }
+
+}
